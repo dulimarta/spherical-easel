51659aef78b95203f7e502fea625e9b752e9c0df
Hans Dulimarta <HansDulimarta@gvsu.edu>
2020-07-30 17:00:15 -0400

    SENoduleTree: add icon to measurement objects, highlight the associated segment when mouseover length measurement.

--- src/components/SENoduleTree.vue
+++ src/components/SENoduleTree.vue
@@ -2,29 +2,32 @@
   <div>
     <!-- <span v-for="c in points" :key="c.id">{{c.name}}</span> -->
     <div id="topContainer" :style="indent">
-      <div id="nodeContent" :class="nodeOrLabel" @mouseenter="glowMe(true)"
-        @mouseleave="glowMe(false)">
-        <v-icon v-if="isPoint">mdi-vector-point
-        </v-icon>
-        <v-icon v-else-if="isLineSegment">mdi-vector-radius
-        </v-icon>
-        <v-icon v-else-if="isLine">mdi-vector-line
-        </v-icon>
+      <div
+        id="nodeContent"
+        :class="nodeOrLabel"
+        @mouseenter="glowMe(true)"
+        @mouseleave="glowMe(false)"
+      >
+        <v-icon v-if="isPoint">mdi-vector-point</v-icon>
+        <v-icon v-else-if="isLineSegment">mdi-vector-radius</v-icon>
+        <v-icon v-else-if="isLine">mdi-vector-line</v-icon>
         <v-icon v-else-if="isCircle">
           mdi-vector-circle-variant
         </v-icon>
         <v-icon v-else-if="isIntersectionPoint">
           mdi-vector-intersection
         </v-icon>
-        <span class="contentText"
-          v-if="label && label.length > 0">{{label}}</span>
+        <v-icon v-else-if="isMeasurement">mdi-tape-measure</v-icon>
+        <span class="contentText" v-if="label && label.length > 0">
+          {{ label }}
+        </span>
         <v-tooltip v-else right>
-          <template v-slot:activator="{on}">
+          <template v-slot:activator="{ on }">
             <div class="contentText ml-1" v-on="on" :class="showClass">
-              {{prettyName}}
+              {{ prettyName }}
             </div>
           </template>
-          <span>{{definitionText}}</span>
+          <span>{{ definitionText }}</span>
         </v-tooltip>
         <div v-show="isPlottable" @click="toggleVisibility" class="mr-2">
           <v-icon small v-if="isHidden">
@@ -34,8 +37,7 @@
             mdi-eye-off
           </v-icon>
         </div>
-        <v-btn small v-show="hasExistingChildren"
-          @click="expanded = !expanded">
+        <v-btn small v-show="hasExistingChildren" @click="expanded = !expanded">
           <v-icon v-if="!expanded">mdi-chevron-right</v-icon>
           <v-icon v-else>mdi-chevron-down</v-icon>
         </v-btn>
@@ -44,9 +46,13 @@
       <transition name="slide-right">
         <div v-show="expanded">
           <!-- Recursive component here -->
-          <SENoduleTree v-for="(n,pos) in existingChildren" :key="pos"
-            :children="n.kids" :depth="depth + 1" :node="n">
-          </SENoduleTree>
+          <SENoduleTree
+            v-for="(n, pos) in existingChildren"
+            :key="pos"
+            :children="n.kids"
+            :depth="depth + 1"
+            :node="n"
+          ></SENoduleTree>
         </div>
       </transition>
     </div>
@@ -54,25 +60,25 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component';
-import { Prop } from 'vue-property-decorator';
-import { SENodule } from '../models/SENodule';
-import { SEIntersectionPoint } from '../models/SEIntersectionPoint';
-import { SEPoint } from '../models/SEPoint';
-import { SELine } from '../models/SELine';
-import { SESegment } from '@/models/SESegment';
-import { SECircle } from '../models/SECircle';
-import { SEMeasurement } from '@/models/SEMeasurement';
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+import { SENodule } from "../models/SENodule";
+import { SEIntersectionPoint } from "../models/SEIntersectionPoint";
+import { SEPoint } from "../models/SEPoint";
+import { SELine } from "../models/SELine";
+import { SESegment } from "@/models/SESegment";
+import { SECircle } from "../models/SECircle";
+import { SEMeasurement } from "@/models/SEMeasurement";
+import { SELength } from "@/models/SELength";
 
 @Component({})
 export default class SENoduleTree extends Vue {
+  @Prop()
+  readonly children!: SENodule[];
 
   @Prop()
-  readonly children!: SENodule[]
-
-  @Prop()
-  readonly node?: SENodule
+  readonly node?: SENodule;
 
   @Prop()
   readonly label?: string; /** Wheen defined, label takes over the node name */
@@ -81,66 +87,74 @@ export default class SENoduleTree extends Vue {
   private showChildren!: boolean;
 
   @Prop()
-  readonly depth!: number /** The depth value controls the indentation level */
+  readonly depth!: number; /** The depth value controls the indentation level */
 
   private expanded = false;
 
   mounted(): void {
     if (this.children)
       this.expanded = this.showChildren && this.children.length > 0;
-    else this.expanded = false
+    else this.expanded = false;
   }
 
   toggleVisibility(): void {
     if (this.node) {
-      this.node.showing = !this.node.showing
+      this.node.showing = !this.node.showing;
     }
   }
 
   glowMe(flag: boolean): void {
     if (this.node) {
-      this.node.glowing = flag
+      if (this.isPlottable) this.node.glowing = flag;
+      else if (this.node instanceof SELength) {
+        const target = this.node?.parents[0] as SESegment;
+        target.glowing = flag;
+      }
     }
   }
 
   get nodeOrLabel(): string {
-    return this.node ? "node" : "label"
+    return this.node ? "node" : "label";
   }
   get hasExistingChildren(): boolean {
-    return this.existingChildren.length > 0
+    return this.existingChildren.length > 0;
   }
 
   get isHidden(): boolean {
     return this.node ? !this.node.showing : false;
-
   }
 
   get isPoint(): boolean {
-    return this.node instanceof SEPoint
+    return this.node instanceof SEPoint;
   }
 
   get isLine(): boolean {
-    return this.node instanceof SELine
+    return this.node instanceof SELine;
   }
   get isLineSegment(): boolean {
-    return this.node instanceof SESegment
+    return this.node instanceof SESegment;
   }
   get isCircle(): boolean {
-    return this.node instanceof SECircle
+    return this.node instanceof SECircle;
   }
 
   get isIntersectionPoint(): boolean {
-    return this.node instanceof SEIntersectionPoint
+    return this.node instanceof SEIntersectionPoint;
   }
 
+  get isMeasurement(): boolean {
+    return this.node instanceof SEMeasurement;
+  }
   get isPlottable(): boolean {
-    return this.node instanceof SEPoint ||
+    return (
+      this.node instanceof SEPoint ||
       this.node instanceof SELine ||
       this.node instanceof SESegment ||
       this.node instanceof SECircle
+    );
   }
   get prettyName(): string {
-    return this.label ?? this.name
+    return this.label ?? this.name;
   }
 
   get name(): string {
@@ -148,32 +162,39 @@ export default class SENoduleTree extends Vue {
   }
 
   get showClass(): string {
-    return (this.label || this.node?.showing) ? "visibleNode" : "invisibleNode"
+    return this.label || this.node?.showing ? "visibleNode" : "invisibleNode";
   }
 
   get existingChildren(): SENodule[] {
     return this.children.filter((n: SENodule) => {
-      if (n instanceof SEIntersectionPoint)
-        return n.isUserCreated;
-      else return n.exists
-    })
+      if (n instanceof SEIntersectionPoint) return n.isUserCreated;
+      else return n.exists;
+    });
   }
 
   get indent(): any {
-    return { marginLeft: `${this.depth * 8}px` }
+    return { marginLeft: `${this.depth * 8}px` };
   }
 
   get definitionText(): string {
     if (this.node instanceof SEPoint)
-      return this.node?.name + this.node.locationVector.toFixed(2)
-    else if (this.node instanceof SELine || this.node instanceof SESegment || this.node instanceof SECircle)
-      return this.node?.name + "(" + this.node.parents.map(p => p.name).join(",") + ")";
+      return this.node?.name + this.node.locationVector.toFixed(2);
+    else if (
+      this.node instanceof SELine ||
+      this.node instanceof SESegment ||
+      this.node instanceof SECircle
+    )
+      return (
+        this.node?.name +
+        "(" +
+        this.node.parents.map(p => p.name).join(",") +
+        ")"
+      );
     else if (this.node instanceof SEMeasurement) {
-      const targetSegment = this.node?.parents[0] as SESegment
-      const len = `${(targetSegment.arcLength / Math.PI).toFixed(2)} \u{1D7B9}`
+      const targetSegment = this.node?.parents[0] as SESegment;
+      const len = `${(targetSegment.arcLength / Math.PI).toFixed(2)} \u{1D7B9}`;
       return this.node?.name + `Len(${targetSegment.name}) = ${len}`;
-    } else return "n/a"
-
+    } else return "n/a";
   }
 }
 </script>
