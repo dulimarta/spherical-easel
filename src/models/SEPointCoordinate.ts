175e5eee8729fea1d7e07f7a845706982ed89e0a
Wiliam Dickinson <dickinsw@gvsu.edu>
2021-07-24 20:36:48 -0400

    Minor fix

--- src/models/SEPointCoordinate.ts
+++ src/models/SEPointCoordinate.ts
@@ -46,7 +46,6 @@ export class SEPointCoordinate extends SEExpression {
   public customStyles = (): Set<string> => emptySet;
 
   public get noduleDescription(): string {
-    this.prettyValue;
     switch (this.selector) {
       case CoordinateSelection.X_VALUE:
         return String(
