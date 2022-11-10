8f8cfeb8e8cc792906591e29d1f1503b1e165123
sherewale <sherwale@mail.gvsu.edu>
2022-11-10 16:44:30 -0500

    Sprint 4: Added display for current tool

--- src/components/CurrentToolSelection.vue
+++ src/components/CurrentToolSelection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div >
+  <div>
     <!-- Displays the current tool in the left panel by the collapsible arorw -->
     <div v-if="!toolboxMinified">
       <div class="pa-4"
@@ -8,14 +8,34 @@
           <v-icon class="mr-2"
             :key="Math.random()">$vuetify.icons.values.{{actionMode}}
           </v-icon>
-          <h3>{{$t(`buttons.${activeToolName}`, {}).toString()}}</h3>
+          <!-- Checks if ApplyTransformation is selected and changes the display accordingly. -->
+          <h3 v-if="activeToolName != 'ApplyTransformationDisplayedName'">
+            {{$t(`buttons.${activeToolName}`, {}).toString()}}</h3>
+          <template v-else>
+            <h3>
+              {{$t(`buttons.${activeToolName}`, {}).toString()}}
+              <br>
+              <h4 class="ap"
+                :key="Math.random()">{{applyTransformationText}}</h4>
+            </h3>
+          </template>
         </v-row>
       </div>
       <div v-else>
         <h2>{{$t(`buttons.NoToolSelected`, {}).toString()}}</h2>
       </div>
     </div>
-
+    <!-- Displays the icon and arrow if toolbox is minified -->
+    <div v-else>
+      <div class="pa-4"
+        v-if="activeToolName">
+        <v-row>
+          <v-icon class="mr-3"
+            :key="Math.random()">$vuetify.icons.values.{{actionMode}}
+          </v-icon>
+        </v-row>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +43,8 @@
 import { Vue, Component, Prop, Watch } from "vue-property-decorator";
 import { mapActions, mapState } from "pinia";
 import { useSEStore } from "@/stores/se";
+import i18n from "@/i18n";
+import EventBus from "@/eventHandlers/EventBus";
 
 @Component({
   computed: {
@@ -35,6 +57,23 @@ export default class CurrentToolSelection extends Vue {
 
   readonly activeToolName!: string | undefined;
   private currentToolIcon: string | undefined;
+  private applyTransformationText = i18n.t(`objects.selectTransformation`);
+
+  //The next 3 functions are for the text for the applied transformation.
+  created(): void {
+    EventBus.listen(
+      "set-apply-transformation-footer-text",
+      this.additionalFooterText
+    );
+  }
+  additionalFooterText(e: { text: string }): void {
+    //console.debug("apply transform", e.text);
+    this.applyTransformationText = e.text;
+  }
+
+  beforeDestroy(): void {
+    EventBus.unlisten("set-apply-transformation-footer-text");
+  }
 }
 </script>
 
