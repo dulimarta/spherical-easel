aedfbd58190e7fe723f434fd059575f52eb26993
Wiliam Dickinson <dickinsw@gvsu.edu>
2023-07-09 17:04:43 -0400

    Set Earth mode command attempt

---
+++ src/commands/SetEarthModeCommand.ts
@@ -0,0 +1,55 @@
+import { Command } from "./Command";
+import { SENodule } from "@/models/SENodule";
+import { SavedNames } from "@/types";
+import { useSEStore } from "@/stores/se";
+import { storeToRefs } from "pinia";
+//const seStore = useSEStore();
+//const { isEarthMode } = storeToRefs(seStore);
+
+export class SetEarthModeCommand extends Command {
+  private showing: boolean;
+
+  constructor(showing: boolean) {
+    super();
+    this.showing = showing;
+  }
+
+  do(): void {
+    // isEarthMode.value = this.showing;
+  }
+
+  saveState(): void {}
+
+  restoreState(): void {
+    //isEarthMode.value = !this.showing;
+  }
+
+  toOpcode(): null | string | Array<string> {
+    return [
+      "SetEarthMode",
+      // Specific attributes necessary for recreating earth mode
+      "objectShowing=" + this.showing
+    ].join("&");
+  }
+
+  static parse(command: string, objMap: Map<string, SENodule>): Command {
+    const tokens = command.split("&");
+    const propMap = new Map<SavedNames, string>();
+    // load the tokens into the map
+    tokens.forEach((token, ind) => {
+      if (ind === 0) return; // don't put the command type in the propMap
+      const parts = token.split("=");
+      propMap.set(parts[0] as SavedNames, Command.asciiDecToSymbol(parts[1]));
+    });
+
+    if (propMap.get("objectShowing") !== undefined) {
+      return new SetEarthModeCommand(propMap.get("objectShowing") === "true");
+    } else {
+      throw new Error(
+        `SetEarthModeDisplayCommand:  ${propMap.get(
+          "objectShowing"
+        )} is undefined`
+      );
+    }
+  }
+}
