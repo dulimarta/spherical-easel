3a0f81845c3ee5c16e4cdc4d15dc44a5f93e75c1
novac <novacrays@gmail.com>
2024-10-23 14:33:01 -0400

    baseline SEText

---
+++ src/models/SEText.ts
@@ -0,0 +1,18 @@
+import { ObjectState } from "@/types";
+import i18n from "@/i18n";
+import { Vector3 } from "three";
+import SETTINGS from "@/global-settings";
+const { t } = i18n.global;
+
+export class SEText extends SENodule {
+	public shallowUpdate(): void {
+		//Something goes here
+	}
+
+	public update(
+    objectState?: Map<number, ObjectState>,
+    orderedSENoduleList?: number[]
+    ): void {
+    	//Something goes here
+	}
+}
