50a6c61f09e2c821a154daecf63154a0f5bd6cea
Nathaniel Tabacchi <ntabacchi@outlook.com>
2024-10-30 16:38:40 -0400

    Fixed debug message. Added note of where to fire an EventBus event.

--- src/eventHandlers/TextHandler.ts
+++ src/eventHandlers/TextHandler.ts
@@ -6,7 +6,7 @@ export default class TextHandler extends Highlighter {
   mousePressed(event: MouseEvent): void {
     console.debug("TextHandler::mousePressed()")
     console.debug(`TextHandler: this.currentScreenVector.x = ${this.currentScreenVector.x}`);
-    console.debug(`TextHandler: this.currentScreenVector.y = ${this.currentScreenVector.x}`);
+    console.debug(`TextHandler: this.currentScreenVector.y = ${this.currentScreenVector.y}`);
     const testText = new SEText("Hello World!",
       this.currentScreenVector.x,
       this.currentScreenVector.y
@@ -14,6 +14,8 @@ export default class TextHandler extends Highlighter {
     const TextCmd = new AddTextCommand(testText);
     console.debug("Executing AddTextCommand...");
     TextCmd.execute();
+
+    // fire event like "show text dialog box"
   }
   mouseReleased(event: MouseEvent): void {
     /* None */
