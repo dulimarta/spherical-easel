5413478b1d679057bd075fd347e8e4b68cc79771
Hans Dulimarta <dulimarh@gvsu.edu>
2025-06-05 09:31:30 -0400

    New tab (owner-vs-constructions) in Firebase cleanup page.

--- src/components/FirebaseStorageGarbageCollector.vue
+++ src/components/FirebaseStorageGarbageCollector.vue
@@ -43,10 +43,13 @@
     </v-tabs-window-item>
     <v-tabs-window-item value="User" class="mx-3">
       <h2>User Created Constructions</h2>
-      <v-data-table :items="queryOwner" :headers="userTableHeaders">
-        <template #item.query="{ value }">
-          {{ value.docs.length }} files
-        </template>
+      {{ ownedConstruction.length }} {{ userProfiles.size }}
+      <v-data-table
+        :items="ownedConstruction"
+        :headers="userTableHeaders"
+        item-value="owner"
+        density="compact"
+        hover show-expand>
         <template #item.owner="{ value }">
           <div class="text-h6">
           {{ userProfiles.get(value)?.displayName ?? "<NONAME>" }} / {{ userProfiles.get(value)?.role }} </div>
@@ -55,9 +58,32 @@
             {{ userProfiles.get(value)?.location }}        
             </div>
         </template>
-        <template #item.actions="{item}">
-          <v-icon v-if="item.query.docs.length === 0" @click="deleteUserCollection(item.owner)">mdi-delete</v-icon>
+        <template #item.constructions="{ value }">
+          {{ value.length }} files
         </template>
+        <template #item.data-table-expand="{ internalItem, isExpanded, toggleExpand}">
+          <template v-if="internalItem.columns.constructions.length > 0">
+          <v-icon v-if="isExpanded(internalItem)" @click="toggleExpand(internalItem)">mdi-chevron-up</v-icon>
+          <v-icon v-else @click="toggleExpand(internalItem)">mdi-chevron-down</v-icon>
+          </template>
+        </template>
+        <template #expanded-row="{columns, item}">
+          <!-- <table border="1">
+            <tr><th>PubID</th><th>Author</th><th>Description</th><th>Created</th><th>Star Count</th></tr>
+            <tr v-for="c in item.constructions">
+              <td>{{ c.publicDocId }}</td>
+              <td>{{ c.author }}</td>
+              <td>{{ c.description }}</td>
+              <td>{{ c.dateCreated }}</td>
+              <td>{{ c.starCount }}</td>
+            </tr>
+          </table> -->
+          <v-data-table :items="item.constructions" :headers="constructionHeaders" item-value="description"
+          density="compact"></v-data-table>
+        </template>
+        <!--template #item.actions="{item}">
+          <v-icon >mdi-delete</v-icon>
+        </!--template> -->
       </v-data-table>
     </v-tabs-window-item>
     <!-- <v-data-table :items="constructionDetails" /> -->
@@ -87,7 +113,7 @@ import {
   QuerySnapshot
 } from "firebase/firestore";
 import { UserProfile } from "@/types";
-// import { SphericalConstruction } from "@/types";
+import { ConstructionInFirestore } from "@/types/ConstructionTypes";
 let appStorage: FirebaseStorage;
 let appDB: Firestore;
 
@@ -111,16 +137,24 @@ const tableHeaders = [
 ];
 const userTableHeaders = [
   {
-    title: "Owner", key: "owner"
+    title: "Owner",
+    key: "owner"
   },
   {
-    title: "Constructions Count", key: "query"
+    title: "Constructions Count",
+    key: "constructions"
   },
-  {
-    title: "Actions", key: "actions"
-  }
-]
+  // {
+  //   title: "Actions",
+  //   key: "actions"
+  // }
+];
 
+const constructionHeaders = [
+  {title: "Description", key: "description"},
+  { title: "Created", key: "dateCreated" },
+  {title: "Public", key: "publicDocId"}
+]
 type CloudFile = {
   path: string;
   size: number;
@@ -133,13 +167,15 @@ type Construction = {
   script: string;
 };
 type OwnDocs = {
-  owner: string,
-  query: QuerySnapshot
-}
+  owner: string;
+  constructions: ConstructionInFirestore[];
+};
 const svgFiles: Ref<CloudFile[]> = ref([]);
 const scriptFiles: Ref<CloudFile[]> = ref([]);
-const queryOwner: Ref<OwnDocs[]> = ref([]);
-const userProfiles: Ref<Map<string,UserProfile>> = ref(new Map())
+// const queryOwner: Ref<OwnDocs[]> = ref([]);
+// const userToConstructionMap: Map<string, ConstructionInFirestore[]> = new Map();
+const ownedConstruction: Ref<OwnDocs[]> = ref([]);
+const userProfiles: Ref<Map<string, UserProfile>> = ref(new Map());
 const tab = ref("SVG");
 
 const constructionDetails: Ref<Construction[]> = ref([]);
@@ -149,51 +185,56 @@ onMounted(async () => {
   appDB = getFirestore();
 
   const qsUsers = await getDocs(collection(appDB, "users"));
-  const promiseOwner: Map<string, Promise<QuerySnapshot>> = new Map();
-  const task = qsUsers.docs.map((qdUser: DocumentSnapshot) => {
-    const uDetails = qdUser.data() as UserProfile
-    console.debug(uDetails)
-    userProfiles.value.set(qdUser.id, uDetails)
-    const uCons = collection(qdUser.ref, "constructions");
-    const uProm = getDocs(uCons);
-    promiseOwner.set(qdUser.id, uProm);
-    return uProm;
+  // const promiseOwner: Map<string, Promise<QuerySnapshot>> = new Map();
+  qsUsers.docs.forEach(async (qdUser: DocumentSnapshot) => {
+    const uDetails = qdUser.data() as UserProfile;
+    console.debug(`Profile of ${qdUser.id}`, uDetails);
+    userProfiles.value.set(qdUser.id, uDetails);
+    const uColl = collection(qdUser.ref, "constructions");
+    console.debug("Pulling constructions from ", uColl.path);
+    const uDocs = await getDocs(uColl);
+    const ddd = await uDocs.docs.map(d => d.data() as ConstructionInFirestore);
+    console.debug(`Constructions of ${qdUser.id}`, ddd);
+    ownedConstruction.value.push({ owner: qdUser.id, constructions: ddd });
+    // userToConstructionMap.set(qdUser.id,ddd)
   });
+  // console.debug("After docs.forEach")
+  // ownedConstruction.value = Array.from(userToConstructionMap).map(([owner, val]) =>({owner, constructions: val}))
   const svgSet: Set<string> = new Set();
   const scriptSet: Set<string> = new Set();
-  await Promise.all(task);
-  await promiseOwner.forEach(async (pDocs, owner) => {
-    queryOwner.value.push({ owner, query: await pDocs });
-  });
+  // await Promise.all(task);
+  // await promiseOwner.forEach(async (pDocs, owner) => {
+  //   queryOwner.value.push({ owner, query: await pDocs });
+  // });
 
-  queryOwner.value
-    .filter(z => z.query.docs.length > 0)
-    .flatMap(z => z.query.docs)
-    .map(qds => {
-      const details = qds.data();
-      let preview = "N/A";
-      let script = "N/A";
-      if (details.preview.startsWith("https://")) {
-        const p1 = details.preview.indexOf("%2F");
-        const p2 = details.preview.indexOf("?");
-        preview = details.preview.substring(p1 + 3, p2);
-        svgSet.add(preview);
-      }
-      if (details.script.startsWith("https://")) {
-        const s1 = details.script.indexOf("%2F");
-        const s2 = details.script.indexOf("?");
-        script = details.script.substring(s1 + 3, s2);
-        scriptSet.add(script);
-      }
-      return {
-        docId: qds.id,
-        preview,
-        script
-      } as Construction;
-    })
-    .filter((c: Construction) => c.preview !== "N/A" || c.script !== "N/A");
-  console.debug("SVG set", svgSet);
-  console.debug("Script set", scriptSet);
+  // queryOwner.value
+  //   .filter(z => z.query.docs.length > 0)
+  //   .flatMap(z => z.query.docs)
+  //   .map(qds => {
+  //     const details = qds.data();
+  //     let preview = "N/A";
+  //     let script = "N/A";
+  //     if (details.preview.startsWith("https://")) {
+  //       const p1 = details.preview.indexOf("%2F");
+  //       const p2 = details.preview.indexOf("?");
+  //       preview = details.preview.substring(p1 + 3, p2);
+  //       svgSet.add(preview);
+  //     }
+  //     if (details.script.startsWith("https://")) {
+  //       const s1 = details.script.indexOf("%2F");
+  //       const s2 = details.script.indexOf("?");
+  //       script = details.script.substring(s1 + 3, s2);
+  //       scriptSet.add(script);
+  //     }
+  //     return {
+  //       docId: qds.id,
+  //       preview,
+  //       script
+  //     } as Construction;
+  //   })
+  //   .filter((c: Construction) => c.preview !== "N/A" || c.script !== "N/A");
+  // console.debug("SVG set", svgSet);
+  // console.debug("Script set", scriptSet);
   const svgRef = storageRef(appStorage, "construction-svg");
   const svgResult = await listAll(svgRef);
   const z = await svgResult.items.map(async (aFile: StorageReference) => {
@@ -232,14 +273,14 @@ function deleteFromStorage(
 }
 
 function deleteUserCollection(uid: string) {
-  const uDoc = doc(appDB, "users", uid)
-  console.debug(`Removing user ${uid}`)
-  deleteDoc(uDoc).then(() => { 
-    userProfiles.value.delete(uid)
-    const pos = queryOwner.value.findIndex(x => x.owner == uid)
-    if (pos >= 0) {
-      queryOwner.value.splice(pos, 1)
-    }
-  })
+  const uDoc = doc(appDB, "users", uid);
+  console.debug(`Removing user ${uid}`);
+  deleteDoc(uDoc).then(() => {
+    userProfiles.value.delete(uid);
+    // const pos = queryOwner.value.findIndex(x => x.owner == uid)
+    // if (pos >= 0) {
+    //   queryOwner.value.splice(pos, 1)
+    // }
+  });
 }
 </script>
