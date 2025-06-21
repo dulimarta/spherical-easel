c33cd8606d9bef0fd8475b1d74471f46064f082c
Hans Dulimarta <dulimarh@gvsu.edu>
2025-06-21 08:56:13 -0400

    Add Google Analytics

--- src/main.ts
+++ src/main.ts
@@ -13,24 +13,28 @@ import { Command } from "@/commands/Command";
 import { useSEStore } from "@/stores/se";
 import MouseHandler from "./eventHandlers/MouseHandler";
 import Nodule from "./plottables/Nodule";
+import { createGtag } from "vue-gtag";
 import { SENodule } from "@/models/SENodule";
 const firebaseApp = initializeApp(firebaseConfig);
 const pinia = createPinia();
 
-
 const app = createApp(App);
-const qp = location.search.split(/[?&]/).filter(s => s.length > 0)
-const fPos = qp.findIndex(z => z.startsWith("features"))
+const qp = location.search.split(/[?&]/).filter(s => s.length > 0);
+const fPos = qp.findIndex(z => z.startsWith("features"));
 // To access the beta features use http://localhost:8080/?&features=beta
 if (fPos >= 0) {
-  const [key, value] = qp[fPos].split("=")
-  app.provide(key, value)
+  const [key, value] = qp[fPos].split("=");
+  app.provide(key, value);
 } else {
-  app.provide("features", null)
+  app.provide("features", null);
 }
+const gaTag = createGtag({
+  tagId: "G-1XK98KQMYZ"
+});
 app.use(vuetify);
 app.use(router);
 app.use(i18n);
+app.use(gaTag);
 // When .use(pinia) is NOT the last call, Pinia did not show up in VueJS DevTools
 // https://stackoverflow.com/questions/77456631/why-cant-i-see-pinia-in-vue-devtools
 app.use(pinia);
