09559597d2b3416f5852d81dfb9233c4c2191b4a
Anderson Yeakey <yeakeya@gvsu.edu>
2025-09-17 20:35:12 -0400

    Added skeleton for Circle tests.

---
+++ src/plottables/__tests__/circle.spec.ts
@@ -0,0 +1,144 @@
+import Circle from "@/plottables/Circle.ts";
+import { SphericalConstruction } from "@/types/ConstructionTypes";
+import { createWrapper } from "$/vue-helper";
+import { mockedStore } from "$/mock-utils";
+import { Matrix4 } from "three";
+import { vi } from "vitest";
+import { useAccountStore } from "@/stores/account";
+import { useConstructionStore } from "@/stores/construction";
+import { VueWrapper } from "@vue/test-utils";
+import { TestingPinia } from "@pinia/testing";
+
+describe("Circle: circle doesn't exist", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+
+    it("is not a plottable", () => {
+
+    });
+
+    it("has no attributes", () => {
+
+    });
+});
+
+describe("Circle: circle constructor generates a valid circle plottable", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+});
+
+describe("Circle: updateDisplay properly updates TwoJS objects", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+});
+
+describe("Circle: centerVector getter and setter properly read and update circle center vector", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+});
+
+describe("Circle: circleRadius getter and setter properly read and update circle radius", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+});
+
+describe("Circle: glowingDisplay sets circle to glowing display style", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+});
+
+describe("Circle: normalDisplay sets circle to normal display style", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+});
+
+describe("Circle: setVisible sets circle visible or not", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+});
+
+describe("Circle: addToLayers properly arranges parts to correct layers", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+});
+
+describe("Circle: removeFromLayers properly removes parts from all layers", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+});
+
+describe("Circle: toSVG properly ports circle to an SVG", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+});
+
+describe("Circle: defaultStyleState properly sets default style", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+});
+
+describe("Circle: adjustSize properly sets stroke width", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+});
+
+describe("Circle: stylize sets proper style", () => {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+});
