1653a6c1dde39754518e7a0319ce392027ca35b8
Hans Dulimarta <dulimarh@gvsu.edu>
2026-08-04 23:32:06 -0400

    Generate "fat-line" using LineGeometry & Line2NodeMaterial

--- src/plottables-spherical/SLine.ts
+++ src/plottables-spherical/SLine.ts
@@ -5,31 +5,46 @@ import {
   TorusGeometry,
   Mesh,
   MeshStandardNodeMaterial,
-  Line2NodeMaterial
+  Line2NodeMaterial,
+  ArcCurve,
+  Vector3
 } from "three/webgpu";
 import { Line2 } from "three/addons/lines/webgpu/Line2.js";
+// import { LineSegments2 } from "three/examples/jsm/lines/webgpu/LineSegments2.js";
+// import { LineSegmentsGeometry } from "three/examples/jsm/Addons.js";
 import { LineGeometry } from "three/addons/lines/LineGeometry.js";
+import { color, Fn } from "three/tsl";
 export class SLine extends Nodule<CKLine> {
-  private _lineMesh: Mesh;
-  private _lineMaterial: NodeMaterial;
+  // private _lineMesh: Mesh;
+  // private _lineMaterial: NodeMaterial;
   constructor(name: string, modelRef: CKLine, infiniteLine: boolean) {
     super(name, modelRef);
 
-    this._lineMaterial = new MeshStandardNodeMaterial({ color: 0xffff00 });
-    this._lineMesh = new Mesh(
-      new TorusGeometry(1, 0.006, 20, 120),
-      this._lineMaterial
-    );
-    this.viewGroup.add(this._lineMesh);
+    // this._lineMaterial = new MeshStandardNodeMaterial({ color: 0xffff00 });
+    // this._lineMesh = new Mesh(
+    //   new TorusGeometry(1, 0.006, 20, 120),
+    //   this._lineMaterial
+    // );
+    // this.viewGroup.add(this._lineMesh);
 
+    const circleCurve = new ArcCurve(0, 0, 1, 0, 2 * Math.PI, true);
     const l2geometry = new LineGeometry();
-    l2geometry.setPositions([0, 0, 0, 1, 1, 1]);
+    // l2geometry.setPositions(
+    //   circleCurve.getPoints(20).flatMap(p => [p.x, p.y, 0])
+    // );
+    l2geometry.setFromPoints(circleCurve.getPoints(60));
+    l2geometry.setColors(
+      Array.from({ length: 120 }, () => [0, 15, 200]).flatMap(z => z)
+    );
     const l2material = new Line2NodeMaterial({
-      color: 0x00ff00,
-      linewidth: 3,
+      vertexColors: true,
+      linewidth: 5,
+      dashed: false,
       worldUnits: false
     });
     const ls = new Line2(l2geometry, l2material);
+    ls.computeLineDistances();
+
     this.viewGroup.add(ls);
   }
   show(): void {
@@ -52,6 +67,7 @@ export class SLine extends Nodule<CKLine> {
       line[1],
       -line[0]
     );
-    this._lineMesh.lookAt(-line[2], line[1], -line[0]);
+    // this._lineMesh.lookAt(-line[2], line[1], -line[0]);
+    this.viewGroup.lookAt(-line[2], line[1], -line[0]);
   }
 }
