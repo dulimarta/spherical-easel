d1ab6f0d62badbf1bbf354d984cefdf3909b7563
Hans Dulimarta <dulimarh@gvsu.edu>
2026-09-09 23:08:21 -0400

    Shift the step function for rendering gridlines on the hyperbolid sheets.

--- src/plottables-hyperbolic/MeshFactory.ts
+++ src/plottables-hyperbolic/MeshFactory.ts
@@ -1162,9 +1162,9 @@ export function createHyperboloidSheet(upper: boolean): THREE.Mesh {
     // Alternate method for drawing radial lines
     const radialAngle = atan(positionLocal.y, positionLocal.x);
     // Draw six radial lines on each half of the circle
-    const lineFlag = radialAngle.div(Math.PI).mul(6).fract().step(0.99);
+    const lineFlag = radialAngle.div(Math.PI).mul(6).fract().step(0.02);
     // Draw one horizontal circle at 0.5 increment
-    const heightFlag = positionLocal.z.mul(2).add(0.025).fract().step(0.99);
+    const heightFlag = positionLocal.z.mul(2).add(0.025).fract().step(0.02);
     // lineFlag.toVar().debug();
     // heightFlag.debug();
     const surfaceColor = select(
